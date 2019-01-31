72437c95c6dd478ff094a7f24832c6fb49c92c2e
Andrew Jones <joneseyit@protonmail.com>
2019-01-31 11:45:16 -0500

    sync#2 got login and failed login to work

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-  skip_before_action :authorized, only: [:create]
+  # skip_before_action :authorized, only: [:create]
 
   def show
     @user = User.find(params[:id])
@@ -12,7 +12,6 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-
       @user = User.new(user_params)
       if @user.save
         render json: @user, status: :created
