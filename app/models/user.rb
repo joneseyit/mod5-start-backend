aaed88ca461e5752fa06cbe762a51596b8962ca7
Andrew Jones <joneseyit@protonmail.com>
2019-02-11 16:22:28 -0500

    trying to get heroku up

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { minimum: 2}
   validates :email, presence: true, uniqueness: true
   validates :username, presence:true, uniqueness: { case_sensitive: false }, length: {minimum: 5}
+  validates :password, presence:true, length: { minimum: 6, maximum: 20, too_short: 'At least 6 characters are required for password'}
   has_secure_password
   has_many :photos
 end
